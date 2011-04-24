fb8cfe4a540de3df6ae18f96a5d61059f0b0d6d8
Hannes Landstedt <github.com@zarac.se>
2011-04-24 15:05:52 +0200

    Graph assignment, just starting...

---
+++ tillampadeDatastrukturerOchAlgoritmer/Uppgift2/src/steinerland/Steinerland.java
@@ -0,0 +1,70 @@
+package steinerland;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import graph.WDGDijkstra;
+import graph.WeightedDirectedGraph;
+
+public class Steinerland {
+    protected WeightedDirectedGraph graph;
+
+    public Steinerland() {
+        graph = new WDGDijkstra();
+    }
+
+    public String search(String from, String hours, String minutes, String to)
+    {
+        return "Just go to the station and wait for the next train. It'll be there \"soon\".";
+    }
+
+    public void addRoute(String from, String hoursDeparture, String minutesDeparture, String to, String hoursArrival, String minutesArrival, String train)
+    {
+    }
+
+    public void loadFile(String path)
+    {
+        try
+        {
+            FileReader fileReader = new FileReader(path);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            // assuming format:
+            // <train>
+            // <departureCity>:<departureTime>-<arrivalCity>:<arrivalTime>
+            //
+            // example:
+            // high-speed commuter train 0501
+            // Steinerstadt: 05.40 - El Seco: 06.03
+            // El Seco: 06.05 - Neubergstadt: 06.39
+            // Neubergstadt: 06.41 - Steinerstadt: 06:58
+            String line = bufferedReader.readLine();
+            while (line != null) 
+            {
+                String train = "Unknown train";
+                // the formatting in the time table file is shady, it's not uniformed..
+                String[] parts = line.split("[.-:]");
+                System.out.println(parts);
+                if (parts.length == 6)
+                    addRoute(parts[0].trim(), parts[1].trim(), parts[2].trim(), parts[3].trim(), parts[4].trim(), parts[5].trim(), train);
+                else
+                    train = line;
+                line = bufferedReader.readLine();
+            }
+
+            bufferedReader.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("[ERROR] Exception: " + e);
+        }
+    }
+
+    public static void main( String[] args ) {
+        Steinerland steinerland = new Steinerland();
+
+        steinerland.loadFile("data/timetable.tbl");
+        new Gui(steinerland);
+    }
+}
